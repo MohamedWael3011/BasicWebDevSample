8f2f607f60c0b2f2ad2305cb2ba30d6dad96e36d
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2023-08-19 02:53:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+DownArrowBtn = document.getElementById("move-down")
+
+DownArrowBtn.addEventListener("mouseover", ()=>{
+DownArrowBtn.classList.add("bx-fade-down")
+});
+
+DownArrowBtn.addEventListener("mouseleave", ()=>{
+    DownArrowBtn.classList.remove("bx-fade-down")
+    });
+
+    
